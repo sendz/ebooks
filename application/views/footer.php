4c4b855223990a4da301e8616ae700e5553941ba
sendz <sendzation@gmail.com>
2014-02-25 06:33:02 +0700

    Add HTML Footer file

---
+++ application/views/footer.php
@@ -0,0 +1,6 @@
+	<script type="text/javascript" src="<?php echo base_url();?>js/jquery-2.0.3.min.js"></script>
+	<script type="text/javascript" src="<?php echo base_url();?>js/bootstrap.min.js"></script>
+	<script type="text/javascript">
+	</script>
+</body>
+</html>
