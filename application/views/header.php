024cf0d1d09fb7ed13577c48d6fad75113cf8c90
sendz <sendzation@gmail.com>
2014-02-25 06:30:07 +0700

    Add HTML Header file

---
+++ application/views/header.php
@@ -0,0 +1,13 @@
+<!doctype HTML>
+<html>
+<head>
+	<title>eBooks</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/css/bootstrap.css">
+</head>
+	<body>
+		<div class="jumbotron">
+			<div class="container">
+				<h1>eBooks</h1>
+				<p>eBooks manager</p>
+			</div>
+		</div>
