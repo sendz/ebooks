7ab6ce05e856855270b98d57dc106e0b5a8080dd
sendz <sendzation@gmail.com>
2014-02-25 19:44:15 +0700

    Fix if logic for empty links

--- application/views/book.php
+++ application/views/book.php
@@ -48,10 +48,23 @@
 											<td>
 												<div class='dropdown'>
 													<a data-toggle='dropdown' class='btn btn-info dropdown-toggle' href='#'>Download Links <span class='caret'></span></a>
-													<ul class='dropdown-menu'>
+													<ul class='dropdown-menu'>";
+								if ($data->file) {
+									echo "
 															<li><a href='$data->file'>Direct Link</a></li>
+										";
+								}
+								if ($data->mirror1) {
+									echo "
 															<li><a href='$data->mirror1'>Mirror Link</a></li>
+										";
+								}
+								if ($data->mirror2) {
+									echo "
 															<li><a href='$data->mirror2'>Mirror Link</a></li>
+										";
+								}
+								echo "
 													</ul>
 												</div>
 											</td>
--- application/views/select_category.php
+++ application/views/select_category.php
@@ -1,24 +1,36 @@
-
-	<?php
-		if ($category) {
-			foreach ($category as $data) {
-				echo "	<tr>
-							<td>$data->date</td>
-							<td>$data->user</td>
-							<td>$data->title</td>
-							<td>$data->category</td>
-							<td>
-								<div class='dropdown'>
-									<a data-toggle='dropdown' class='btn btn-info dropdown-toggle' href='#'>Download Links <span class='caret'></span></a>
-									<ul class='dropdown-menu'>
-											<li><a href='$data->file'>Direct Link</a></li>
-											<li><a href='$data->mirror1'>Mirror Link</a></li>
-											<li><a href='$data->mirror2'>Mirror Link</a></li>
-									</ul>
-								</div>
-							</td>
-						</tr>
-				";
+<?php
+	if ($category) {
+		foreach ($category as $data) {
+			echo "	<tr>
+						<td>$data->date</td>
+						<td>$data->user</td>
+						<td>$data->title</td>
+						<td>$data->category</td>
+						<td>
+							<div class='dropdown'>
+								<a data-toggle='dropdown' class='btn btn-info dropdown-toggle' href='#'>Download Links <span class='caret'></span></a>
+								<ul class='dropdown-menu'>";
+			if ($data->file) {
+				echo "
+										<li><a href='$data->file'>Direct Link</a></li>
+					";
 			}
+			if ($data->mirror1) {
+				echo "
+										<li><a href='$data->mirror1'>Mirror Link</a></li>
+					";
+			}
+			if ($data->mirror2) {
+				echo "
+										<li><a href='$data->mirror2'>Mirror Link</a></li>
+					";
+			}
+			echo "
+								</ul>
+							</div>
+						</td>
+					</tr>
+			";
 		}
-	?>
+	}
+?>
