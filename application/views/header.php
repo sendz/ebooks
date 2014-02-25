d69d2c8c1a322b3c089f5f2572406835377f0292
sendz <sendzation@gmail.com>
2014-02-25 19:24:55 +0700

    Fix css link

--- application/views/header.php
+++ application/views/header.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<title>eBooks</title>
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/bootstrap.css">
 </head>
 	<body>
 		<div class="jumbotron">
