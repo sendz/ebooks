13108004c72dfa1f95bdb3498cb4cac2ed46094d
sendz <sendzation@gmail.com>
2014-03-05 23:04:09 +0700

    Add some JS

--- application/views/footer.php
+++ application/views/footer.php
@@ -14,6 +14,21 @@
 			    }
 			});
         });
+        $(function() {
+		// Setup drop down menu
+			$('.dropdown-toggle').dropdown();
+			// Fix input element click problem
+			$('.dropdown-menu input, .dropdown-menu label').click(function(e) {
+				e.stopPropagation();
+			});
+		});
+		$('#auth_modal').modal({
+			backdrop: 'static',
+			show: 'true',
+			keyboard: 'false'
+		});
+		var credential = localStorage.credential;
+		document.getElementById("credential").innerHTML = credential;
 	</script>
 </body>
 </html>
