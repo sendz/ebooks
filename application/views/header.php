dfb8bd9125c2f6f9e4f34458bcb0da5b20256945
sendz <sendzation@gmail.com>
2014-03-08 15:13:41 +0700

    Split header and jumbotron

--- application/views/header.php
+++ application/views/header.php
@@ -5,9 +5,3 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/bootstrap.css">
 </head>
 	<body>
-		<div class="jumbotron">
-			<div class="container">
-				<h1>eBooks</h1>
-				<p>eBooks manager</p>
-			</div>
-		</div>
